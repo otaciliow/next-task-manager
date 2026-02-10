90c4f9d8d80fadc18826c0420337b66c8c81ca89
otaciliow <otacilio.dml@hotmail.com>
2026-02-10 10:28:54 -0300

    refactor: conditional structure

--- src/components/tasksList.tsx
+++ src/components/tasksList.tsx
@@ -10,19 +10,21 @@ export function TasksList() {
 
     return (
         <>
-            <ul className="flex flex-col gap-2">
+            <ul className="flex flex-col gap-3">
 
-                {tasks.length < 1 && <div>Você ainda não cadastrou nenhuma tarefa para exibir aqui...</div>}
-
-                {tasks && tasks.map((task) => (
-                    <li key={task.id} className={`flex justify-between gap-5 w-100 ${task.completed ? 'line-through text-zinc-500' : ''}`}>
-                        {task.title}
-                        <div className="flex gap-1">
-                            <SquareCheck className={`cursor-pointer hover:text-green-400 transition-all duration-125 ${task.completed ? 'text-green-500' : ''}`} onClick={() => toggleTask(task.id)} />
-                            <Trash2 className="cursor-pointer hover:text-red-400 transition-all duration-125" onClick={() => deleteTask(task.id)} />
-                        </div>
-                    </li>
-                ))}
+                {tasks.length == 0 
+                    ? 
+                        <div>Você ainda não cadastrou nenhuma tarefa para exibir aqui...</div> 
+                    :
+                        tasks.map((task) => (
+                        <li key={task.id} className={`flex justify-between gap-5 w-100 ${task.completed ? 'line-through text-zinc-500' : ''}`}>
+                            {task.title}
+                            <div className="flex gap-1 items-center">
+                                <SquareCheck className={`cursor-pointer hover:text-green-400 transition-all duration-125 ${task.completed ? 'text-green-500' : ''}`} onClick={() => toggleTask(task.id)} />
+                                <Trash2 className="cursor-pointer hover:text-red-400 transition-all duration-125" onClick={() => deleteTask(task.id)} />
+                            </div>
+                        </li>
+                    ))}
             </ul>
         </>
     )
