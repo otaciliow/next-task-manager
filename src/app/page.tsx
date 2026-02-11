551380129d2ad1e949a058446419964f1b596e4f
otaciliow <otacilio.dml@hotmail.com>
2026-02-11 10:23:37 -0300

    fix: page's title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 dark:bg-black">
       <main className="flex flex-col justify-center items-center gap-3 border-purple-500 rounded-md border p-5">
-        <h1 className="font-bold text-2xl">Task Manager - React</h1>
+        <h1 className="font-bold text-2xl">Task Manager - Next</h1>
         <h2>Seu gerenciador de tarefas pessoal! Para adicionar uma nova tarefa, basta digitar sua descrição abaixo e clicar no botão!</h2>
         <MainForm />
         <TasksList />
